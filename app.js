7537551ea6a77113977ed712775d89a03f908248
Ravindra Rawat <ravindrarawat@Ravindras-MacBook-Air.local>
2017-12-11 12:38:00 -0700

    pagination final

--- app.js
+++ app.js
@@ -1,78 +1,129 @@
 
-// const studentList contains the details 0r list of students.
-const studentItem=() => {
-  const studentDetails = document.getElementsByClassName('student-item');
-  return studentDetails ;
-};
+// arrays to store the student details
 
-// Number of students in the list
-const studentCount = studentItem().length;
+const originalStudentList = document.getElementsByClassName('student-item');
+const studentDetails = document.getElementsByClassName('student-item');
+const matchedStudentsList = [];
 
-// Number of pages based on length of studentList
-const pageCount = () => {
-return Math.ceil((studentCount/10)) ;
+const pageDiv = document.getElementsByClassName('page')[0];
 
-};
+// showPage function to display 10 students per page , works in reference to page link clicked
 
-//Shows the page with 10 students based on page number
-
-const showPage = (pageNumber)=>{
-
-
-
-for(let i=0;i<studentCount;i++){
-
-    studentItem()[i].style.display = 'none' ;
-
-
-
+function showPage(pageNumber,studentList){
+  for (let i = 0;i < studentList.length;i++ ){
+    studentList[i].style.display = 'none';
     if(i >= (pageNumber-1)*10  &&  i < (pageNumber*10)){
-
-      studentItem()[i].style.display = 'block' ;
-
-    }else{}
+      studentList[i].style.display = 'block' ;
 
     }
-
-  };
-
-// Function to append the page links
-
-  function appendPageLinks(){
-    const pageDiv = document.getElementsByClassName('page');
-    const newDiv = document.createElement('div');
-    const newUl = document.createElement('ul');
-
-    pageDiv[0].removeChild(pageDiv[0].lastChild);
-
-    newDiv.appendChild(newUl);
-    newDiv.className="pagination";
-
-
-  for(let i = 0 ; i < pageCount();i++){
-
-    const newLi = document.createElement('li');
-    const newA = document.createElement('a');
-     newUl.appendChild(newLi);
-     newLi.appendChild(newA);
-     newA.textContent = i+1 ;
-
-     newA.addEventListener('click',()=>{
-      showPage(i+1) ;
-
-     })
-
   }
-    pageDiv[0].appendChild(newDiv);
-    document.getElementsByTagName('a')[0].className ='active';
+}
+
+// function to create links and append them to the page's html
+
+function appendPageLinks(studentList){
+  let pages = Math.ceil(studentList.length/10);
+  //const pageDiv = document.getElementsByClassName('page')[0];
+  const comment = pageDiv.lastChild;
+  pageDiv.removeChild(comment);
+
+  let div = document.createElement('div');
+  let ul = document.createElement('ul');
+  div.className = 'pagination';
+  div.appendChild(ul);
+
+  for(let i =0;i < pages;i++){
+    let li = document.createElement('li');
+    let atag = document.createElement('a');
+    atag.textContent = i+1;
+    atag.addEventListener('click',()=>{showPage(i+1,studentList)});
+    li.appendChild(atag);
+    ul.appendChild(li);
+  }
+  pageDiv.appendChild(div);
+  ul.firstChild.className = 'active';
+  showPage(1,studentList);
+}
+
+// call the function to append the links to page and show the first page
+appendPageLinks(originalStudentList);
+//showPage(1,originalStudentList);
+
+// Create Search Bar
+
+const headerDiv = document.getElementsByClassName('page-header')[0];
+const commnet = headerDiv.lastChild;
+const searchDiv = document.createElement('div');
+const searchInput = document.createElement('input');
+const searchButton = document.createElement('button');
+searchButton.addEventListener('click',() => {
+  search()});
+
+searchDiv.appendChild(searchInput);
+searchDiv.appendChild(searchButton);
+headerDiv.removeChild(commnet);
+headerDiv.appendChild(searchDiv);
+
+
+searchDiv.className = 'student-search';
+searchInput.placeholder = 'Search for students';
+searchButton.textContent = 'Search';
+
+//search bar complete
+
+
+
+// function to show search results
+
+function search(){
+  const studentNames = document.getElementsByTagName('h3');
+  const studentEmails = document.getElementsByClassName('email');
+
+  for (let i = 0 ; i < studentDetails.length;i++){
+    studentDetails[i].style.display = 'block';
+    let name = studentNames[i].innerText;
+    let email = studentEmails[i].innerText;
+
+
+    if(name.indexOf(searchInput.value.toLowerCase()) > -1 || email.indexOf(searchInput.value.toLowerCase()) > -1){
+      matchedStudentsList.push(studentDetails[i]);
+    }else{
+      studentDetails[i].style.display = 'none' ;
+    }
+  }
+
+  searchButton.innerHTML = 'Go Back';
+  searchButton.style.color = 'red';
+  searchButton.addEventListener('click',()=>{window.location.reload()})
+
+  if(matchedStudentsList.length === 0 ){
+      let message = document.getElementsByTagName('h2')[0];
+      message.innerHTML = "NO STUDENTS FOUND";
+      pageDiv.removeChild(pageDiv.lastChild);
+  }else if(matchedStudentsList.length < 10 ){
+    pageDiv.removeChild(pageDiv.lastChild);
+  }else{
+    appendPageLinks(matchedStudentsList);
+  }
 
 }
 
 
 
- pageCount();
- showPage(1);
- appendPageLinks();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
